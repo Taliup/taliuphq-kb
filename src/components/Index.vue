639b235caa399eb14b0223a84e8328dc38dd3e78
Jenish Patel <132077770+Jenish-Patel07@users.noreply.github.com>
2023-08-08 12:08:26 -0400

    Layout Changes
    Changes to knowledge based layout

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,555 +1,172 @@
 <template>
-  <div class="">
-    <div class="side-bar">
-
-      <div class="site-header">
-        <a href="" class="site-title text-decoration-none">
-          <!-- <h4 class="mb-0">Taliup HQ KB</h4> -->
-          <img src="../assets/logo.png" style="height: 55px" alt="">
-          <span class="mx-2 text-dark" style="font-weight: 500">
-            Knowledge Base
-          </span>
-        </a>
-
-        <button id="menu-button" class="site-button btn-reset" @click="showMenu">
-          <i class="fas fa-bars text-accent"></i>
-        </button>
+  <div class="guide-dashboard">
+    <aside class="guide">
+      <div class="logo-container">
+        <img src="../assets/logo.png" alt="Logo" class="logo-image">
+        <span class="guide-title">Knowledge Base</span>
       </div>
-
-      <nav id="site-nav" class="site-nav nav-open">
+      <nav class="guide-nav">
         <ul class="nav-list">
-          <li class="nav-list-item text-left active">
-            <a href="#setting-up" class="nav-list-link w-100">Setting Up Tips</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#quickbooks" class="nav-list-link">ǪuickBooks Integration</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#employees" class="nav-list-link">Employees</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#roles-permissions" class="nav-list-link">Roles and Permissions</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#terminals" class="nav-list-link">Terminals</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#manage-terminals" class="nav-list-link">Manage Terminals</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <a href="#timesheets" class="nav-list-link">Timesheets</a>
-          </li>
-          <li class="nav-list-item text-left">
-            <div class="d-flex flex-row justify-content-between align-items-center">
-              <a href="#catalogs" class="nav-list-link">
-                Catalogs
-              </a>
-              <i class="text-accent mx-3"
-                :class="visibleCatalogs ? 'fas fa-chevron-up' : 'fas fa-chevron-down'"
-               v-b-toggle.catalog-collapse></i>
-            </div>
-
-            <b-collapse id="catalog-collapse" v-model="visibleCatalogs" class="bg-white border-radius">
-              <ul style="list-style-type: none">
-                <li class="nav-list-item text-left">
-                  <a href="#products" class="nav-list-link">Products</a>
-                </li>
-                <li class="nav-list-item text-left">
-                  <a href="#categories" class="nav-list-link">Categories</a>
-                </li>
-                <li class="nav-list-item text-left">
-                  <a href="#inventory" class="nav-list-link">Inventory</a>
-                </li>
-              </ul>
-            </b-collapse>
+          <li v-for="section in sections" :key="section.id">
+            <a :href="'#' + section.id" class="nav-list-link" @click="activateSection(section)">{{ section.name }}</a>
           </li>
         </ul>
       </nav>
+    </aside>
 
-      <!-- <footer class="site-footer">
-        <div class="text-left">
-          This is a site footer.
-        </div>
-      </footer> -->
-    </div>
-
-    <div class="main">
-      <!-- <div class="d-flex flex-row search position-relative">
-        <b-input-group class="px-3">
-          <template #prepend>
-            <i class="fas fa-search search-icon"></i>
-          </template>
-
-          <b-form-input class="border-0 border-bottom rounded-0 search" v-model="searchTerm" placeholder="Search knowledge base" @keypress.enter="handleSearch"></b-form-input>
-        </b-input-group>
-
-        <div v-if="searchResults.length > 0" class="search-results" id="search-results">
-          <ul class="search-results-list">
-            <li v-for="(result, index) in searchResults" :key="index">
-              <strong>{{ result.name }}</strong>:
-              <ul>
-                <li v-for="context in result.contexts" :key="context" v-html="context"></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div> -->
-
-
-      <section id="setting-up" class="p-3">
-        <setting-up ref="settingUpComponent"/>
+    <main class="dashboard">
+      <section v-for="section in sections" :key="section.id" :id="section.id" class="content-section">
+        <component :is="section.component" ref="section.componentRef"></component>
       </section>
-
-      <section id="quickbooks" class="p-3">
-        <quickbooks ref="quickbooksComponent"/>
-      </section>
-
-      <section id="employees" class="p-3">
-        <employees ref="employeesComponent"/>
-      </section>
-
-      <section id="roles-permissions" class="p-3">
-        <roles-and-permissions ref="rolesPermissionsComponent"/>
-      </section>
-
-      <section id="terminals" class="p-3">
-        <terminals ref="terminalsComponent"/>
-      </section>
-
-      <section id="manage-terminals" class="p-3">
-        <manage-terminals ref="manageTerminalsComponent"/>
-      </section>
-
-      <section id="timesheets" class="p-3">
-        <timesheets ref="timesheetsComponent"/>
-      </section>
-
-      <section id="catalogs" class="p-3">
-        <catalogs ref="catalogsComponent"/>
-      </section>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Index',
-  props: {
-    msg: String
-  },
   data() {
     return {
-      searchTerm: '',
-      searchResults: [],
-      visibleCatalogs: false,
-    }
-  },
-  mounted() {
-    const tabHeaders = document.querySelectorAll('.nav-list-item');
-    const siteNav = document.getElementById('site-nav');
-    const menuButton = document.getElementById('menu-button');
-
-    tabHeaders.forEach((header) => {
-      header.addEventListener('click', () => {
-        // Remove active class from all tab headers and panels
-        tabHeaders.forEach((h) => h.classList.remove('active'));
-
-        // Add active class to clicked tab header and panel
-        header.classList.add('active');
-
-        siteNav.classList.remove('nav-open');
-        menuButton.ariaPressed = false;
-      });
-    }); 
-  },
-
-  methods: {
-    showMenu(e) {
-      e.preventDefault();
-      const siteNav = document.getElementById('site-nav');
-      const menuButton = document.getElementById('menu-button');
-
-      if (menuButton.classList.toggle('nav-open')) {
-        siteNav.classList.add('nav-open');
-        menuButton.ariaPressed = true;
-      } else {
-        siteNav.classList.remove('nav-open');
-        menuButton.ariaPressed = false;
-      }
-    },
-
-    handleSearch() {
-      let sections = [];
-
-      let arr = [
+      sections: [
         {
+          id: 'setting-up',
           name: 'Setting Up Tips',
-          ref: this.$refs.settingUpComponent.$refs.settingUpContent,
+          component: 'setting-up',
+          isActive: true,
         },
         {
-          name: 'Quickbooks Integration',
-          ref: this.$refs.quickbooksComponent.$refs.quickbooksContent,
+          id: 'quickbooks',
+          name: 'QuickBooks Integration',
+          component: 'quickbooks',
+          isActive: false,
         },
         {
+          id: 'employees',
           name: 'Employees',
-          ref:this.$refs.employeesComponent.$refs.employeesContent,
+          component: 'employees',
+          isActive: false,
         },
         {
-          name: 'Roles And Permissions',
-          ref: this.$refs.rolesPermissionsComponent.$refs.rolesPermissionsContent,
+          id: 'roles-and-permissions',
+          name: 'Roles and Permissions',
+          component: 'roles-and-permissions',
+          isActive: false,
         },
         {
+          id: 'terminals',
           name: 'Terminals',
-          ref: this.$refs.terminalsComponent.$refs.terminalsContent,
+          component: 'terminals',
+          isActive: false,
         },
         {
+          id: 'manage-terminals',
           name: 'Manage Terminals',
-          ref: this.$refs.manageTerminalsComponent.$refs.manageTerminalsContent,
+          component: 'manage-terminals',
+          isActive: false,
         },
         {
+          id: 'timesheets',
           name: 'Timesheets',
-          ref: this.$refs.timesheetsComponent.$refs.timesheetsContent,
+          component: 'timesheets',
+          isActive: false,
         },
-      ];
-
-      arr.forEach(item => {
-        if (item.ref) {
-          let allText = this.getTextContent(item.ref);
-
-          sections.push({
-            'name': item.name,
-            'content': allText,
-          });
-        }
-      });
-
-      if (!this.searchTerm) {
-        this.searchResults = [];
-        return;
-      }
-
-      const searchTerms = this.searchTerm.trim().split(/\s+/).map(term => `(?=.*\\b${term}\\b)`).join('');
-      const regex = new RegExp(`${searchTerms}.*`, 'gi');
-
-      // this.searchResults = sections.filter((section) => {
-      //   return section.name.match(regex) || section.content.match(regex);
-      // });
-
-      this.searchResults = sections.reduce((results, section) => {
-        const matchingTexts = this.extractContextualText(section.content, regex);
-        if (matchingTexts.length > 0) {
-          results.push({ name: section.name, contexts: matchingTexts });
-        }
-        return results;
-      }, []);
-
-      console.log(this.searchResults);
+        {
+        id: 'catalogs',
+        name: 'Catalogs',
+        subSections: [
+          { id: 'products', name: 'Products', component: 'products', isActive: false },
+          { id: 'categories', name: 'Categories', component: 'categories', isActive: false },
+          { id: 'inventory', name: 'Inventory', component: 'inventory', isActive: false },
+        ],
+        component: 'catalogs',
+        isActive: false,
+        // Add more sections here
+        },
+      ],
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
     },
-
-    getTextContent(element) {
-      let text = "";
-      const nodes = element.childNodes;
-      for (const node of nodes) {
-        if (node.nodeType === Node.TEXT_NODE) {
-          text += node.textContent.trim();
-        } else if (node.nodeType === Node.ELEMENT_NODE) {
-          text += this.getTextContent(node);
-        }
-      }
-      return text;
+    activateSection(section) {
+      this.sections.forEach(sec => (sec.isActive = sec.id === section.id));
     },
-
-    extractContextualText(content, regex) {
-      const words = content.split(/\s+/);
-      const matchingTexts = [];
-      for (let i = 0; i < words.length; i++) {
-        if (regex.test(words[i])) {
-          let startIndex = Math.max(i - 3, 0);
-          let endIndex = Math.min(i + 3, words.length - 1);
-
-          let context = words.slice(startIndex, endIndex + 1).join(' ');
-          context = context.replace(regex, '<span>$&</span>');
-
-          console.log('matchingText for ', words[i] , ' => ', context);
-          matchingTexts.push(context);
-        }
-      }
-      
-
-      return matchingTexts;
-    },
-  }
-}
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.search-results {
-    display: block;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    max-height: calc(100% - 4rem);
-    overflow-y: auto;
-    background-color: #fff;
-    border-bottom-right-radius: 4px;
-    border-bottom-left-radius: 4px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12), 0 3px 10px rgba(0, 0, 0, 0.08);
+.guide-dashboard {
+  display: flex;
+  min-height: 100vh;
+  background-color: #f4f4f4;
 }
 
-.search-results-list {
-    padding-left: 0;
-    margin-bottom: 0.25rem;
-    list-style: none;
-    font-size: 0.875rem !important;
+.guide {
+  flex: 0 0 16rem;
+  padding: 2rem;
+  background-color: #1f1f1f;
+  color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-.highlight {
-  background-color: yellow;
+.logo-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+
+.logo-image {
+  height: 40px;
+}
+
+.guide-title {
+  margin-left: 1rem;
+  font-size: 1.25rem;
   font-weight: bold;
+  color: white;
 }
 
-.input-group-prepend {
-  display: flex;
-  align-items: center;
-}
-
-.form-control:focus {
-  box-shadow: none !important;
-}
-
-@media (min-width: 31.25rem) {
-  .site-title {
-    font-size: 1.5rem !important;
-    line-height: 1.25;
-  }
-}
-
-/* 1064px */
-@media (min-width: 66.5rem) {
-  .side-bar {
-    /* width: calc((100% - 66.5rem) / 2 + 16.5rem); */
-    width: 35vw !important;
-    min-width: 16.5rem;
-  }
-
-  .site-nav, .site-header, .site-footer {
-    width: 19.5rem !important;
-  }
-
-  .main {
-    margin-left: 35vw !important;
-  }
-  
-
-  .bars_button {
-    display: none !important;
-  }
-}
-
-/* 768px */
-@media (min-width: 50rem) {
-  .main {
-    position: relative;
-    max-width: 50rem;
-    margin-left: 15.5rem;
-  }
-
-  .side-bar {
-    flex-flow: column nowrap;
-    position: fixed;
-    width: 15.5rem;
-    height: 100%;
-    border-right: 1px solid #eeebee;
-    align-items: flex-end;
-  }
-
-  .site-nav {
-    display: block !important;
-    padding-top: 3rem;
-    padding-bottom: 1rem;
-    overflow-y: auto;
-    flex: 1 1 auto;
-  }
-
-  .site-title {
-    padding-top: 1.2rem !important;
-    padding-bottom: 1.2rem !important;
-    padding-right: 2rem !important;
-    padding-left: 2rem !important;
-  }
-
-  .site-header .site-button {
-    display: none !important;
-  }
-
-  .nav-list .nav-list-item .nav-list-link {
-    min-height: 2rem;
-    line-height: 1.5rem;
-    padding-right: 2rem;
-    padding-left: 2rem;
-  }
-
-  .site-footer {
-    position: static !important;
-    justify-self: end;
-  }
-
-  .search-results {
-    top: 100%;
-    width: 50rem;
-    max-height: calc(100vh - 200%) !important;
-  }
-}
-
-.site-header img{
-  height: 40px !important;
-}
-
-.site-footer {
-    padding-right: 1rem;
-    padding-left: 1rem;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    color: #959396;
-    font-size: 0.6875rem !important;
-}
-
-.site-nav, .site-header, .site-footer {
-    width: 100%;
-}
-
-.site-nav {
-    display: none;
-}
-
-.site-nav.nav-open {
-    display: block;
-}
-
-.btn-reset {
-    background: none;
-    border: none;
-    margin: 0;
-    text-align: inherit;
-    font: inherit;
-    border-radius: 0;
-    appearance: none;
-}
-
-.site-button {
-    display: flex;
-    height: 100%;
-    padding: 1rem;
-    align-items: center;
-}
-
-.site-header {
-  width: 100%;
-}
-
-.site-header {
-    display: flex;
-    min-height: 3.75rem;
-    align-items: center;
-}
-
-.sitenav {
-  display: none;
-}
-
-.site.nav-open {
-  display: block;
-}
-
-.bars_button {
-  display: flex;
-}
-
-.side-bar {
-    z-index: 0;
-    display: flex;
-    flex-wrap: wrap;
-    background-color: #f5f6fa;
-}
-
-.site-title {
-  padding-right: 1rem;
-  padding-left: 1rem;
-  flex-grow: 1;
-  display: flex;
-  height: 100%;
-  align-items: center;
-  padding-top: 0.75rem;
-  padding-bottom: 0.75rem;
-  color: #27262b;
-  font-size: 1.125rem;
-  border-bottom: 1px solid #eeebee;
-}
-
-.site-title img {
-  height: 30px;
-}
-
-.logo {
-  height: 3.75rem;
-  max-height: 3.75rem;
-  text-decoration: none;
-  border-bottom: 1px solid #eeebee;
-}
-
-.logo a {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  color: #222;
-}
-
-.search {
-  height: 3.75rem;
-  font-size: 14px;
+.guide-nav {
+  margin-bottom: 2rem;
 }
 
 .nav-list {
   padding: 0;
-  margin-top: 0;
-  margin-bottom: 0;
   list-style: none;
-}
-
-.nav-list-item {
-  font-size: 14px;
-  transition: 0.3s ease-in;
-}
-
-.nav-list-item.active {
-  font-weight: 600;
-  background-image: linear-gradient(-90deg, #ebedf5 0%, rgba(235, 237, 245, 0.8) 80%, rgba(235, 237, 245, 0) 100%);
-}
-
-.nav-list-link:hover {
-  background-image: linear-gradient(-90deg, #ebedf5 0%, rgba(235, 237, 245, 0.8) 80%, rgba(235, 237, 245, 0) 100%);
 }
 
 .nav-list-link {
   text-decoration: none;
-  color: #7253ed;
+  color: white;
   display: block;
-  padding: 8px 7px;
+  padding: 0.75rem 0;
+  transition: background-color 0.3s;
 }
 
-.text-accent {
-  color: #7253ed !important;
+.nav-list-link:hover {
+  background-color: rgba(255, 255, 255, 0.1);
 }
 
-.text-left {
+.dashboard {
+  flex: 1;
+  padding: 2rem;
+}
+
+.content-section {
+  margin-bottom: 2rem;
+  background-color: white;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  padding: 2rem;
+}
+
+/* Left align all content */
+.guide,
+.dashboard {
   text-align: left;
 }
 
-section {
-  min-height: 100vh;
-}
+/* Customize styles for other components as needed */
 </style>
---
+++ src/components/Sections/FeatureRelease.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="text-left" ref="featureReleaseContent">
+      <div class="container">
+        <div class="release-header">
+          <h1 class="release-title">Discover What's New</h1>
+          <p class="release-date">Released on August 3rd, 2023</p>
+        </div>
+        <div class="release-content">
+          <div v-for="(feature, index) in features" :key="index" class="feature">
+            <h2 class="feature-title">{{ feature.title }}</h2>
+            <p class="feature-description">{{ feature.description }}</p>
+          </div>
+        </div>
+        <div class="release-footer">
+          <p class="release-conclusion">
+            Stay ahead with these exciting updates. Log in to Taliup Register now and explore the new features!
+          </p>
+          <p class="release-support">For assistance, our support team is just a message away.</p>
+          <p class="release-signature">Best regards,<br />The Taliup Team</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        features: [
+          {
+            title: "Single Login: Streamlined Access Made Easy",
+            description:
+              "Say goodbye to the need for an Owner login – now you can log in with any Employee's username and passcode assigned to a register. The app will automatically adopt the permissions of that Employee account, streamlining the login process and enhancing security.",
+          },
+          {
+            title: "Custom Product Prices: Pricing Flexibility at Your Fingertips",
+            description:
+              "Creating products with custom prices is now a breeze. Simply enable the Custom Price switch when setting up a product, and you can assign any price you wish directly from the Register. It's a game-changer for businesses looking to cater to unique customer preferences and promotions.",
+          },
+          {
+            title: "Filter Schedule by Location: Efficient Appointment Management",
+            description:
+              "The new Filter Schedule by Location feature allows you to view appointments for all locations or filter them by Location (MID). This level of flexibility ensures seamless appointment scheduling and organization, regardless of the number of locations you operate.",
+          },
+          {
+            title: "Introducing Discounts: Save Big on Every Order",
+            description:
+              "Easily create various discounts through Taliup HQ, choosing between Percentage or Fixed Amount discounts and applying them before or after tax. Order Level Discounts make promotions a breeze. Create Custom Discounts for greater flexibility.",
+          },
+          {
+            title: "Catalog Menu Revamp: A Cleaner Look for a Better Experience",
+            description:
+              "Revamped Catalog menu for easy access to all your Catalog items. This cleaner look streamlines your navigation and makes managing your catalog effortless.",
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Your unique CSS styles can go here */
+  .feature-release-page {
+    background-color: #f9fafc;
+    min-height: 100vh;
+    padding: 40px 0;
+  }
+  
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+  
+  .release-header {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  
+  .release-title {
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  .release-date {
+    font-size: 18px;
+    color: #888;
+  }
+  
+  .release-content {
+    margin-top: 20px;
+  }
+  
+  .feature {
+    margin-bottom: 40px;
+  }
+  
+  .feature-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  .feature-description {
+    font-size: 18px;
+    color: #555;
+  }
+  
+  .release-footer {
+    margin-top: 40px;
+    text-align: center;
+  }
+  
+  .release-conclusion {
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  
+  .release-support {
+    font-size: 16px;
+    color: #777;
+    margin-bottom: 10px;
+  }
+  
+  .release-signature {
+    font-size: 16px;
+    color: #444;
+  }
+  </style>
+  
